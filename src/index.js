60ce824cbce51c9918c2acac944bd608dabb27a4
erezpoliak <poliakerez@gmail.com>
2020-08-05 21:20:46 +0300

    init express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+  };
+
+  next();
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
