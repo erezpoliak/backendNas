260e5ac45be20d1cead36c324a537814af1dd207
erezpoliak <poliakerez@gmail.com>
2020-08-05 22:46:39 +0300

    added mockData and post request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import mockDb from "./mockDb";
 
 const app = express();
 
@@ -8,18 +9,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
-  req.context = {
-    models,
-  };
-
-  next();
-});
+const generateSlotNum = () => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 13; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.post("/", (req, res) => {
+  if (mockDb.length < process.env.PARKINGLOTSIZE) {
+    const car = { number: req.body.carNum, slot: generateSlotNum() };
+    mockDb.push(car);
+    return res.send(car.slot);
+  } else {
+    return res.send("parking lot is full");
+  }
 });
---
+++ src/mockDb.js
@@ -0,0 +1,20 @@
+const mockupDb = [
+  {
+    number: "663880992",
+    slot: "77b19082",
+  },
+  {
+    number: "663880945",
+    slot: "888a90123",
+  },
+  {
+    number: "663880908",
+    slot: "888a90123",
+  },
+  {
+    number: "663889032",
+    slot: "888a90123",
+  },
+];
+
+export default mockupDb;
